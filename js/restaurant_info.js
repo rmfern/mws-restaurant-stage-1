56c84790fc9fe3029f5bf8b4ff0feaec5ba68288
Ricardo Fernandez <v-rifern@microsoft.com>
2018-07-22 10:29:23 -0700

    Minor tweaks to mobile first before building up with breakpoints

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,7 +4,7 @@ var newMap;
 /**
  * Initialize map as soon as the page is loaded.
  */
-document.addEventListener('DOMContentLoaded', (event) => {  
+document.addEventListener('DOMContentLoaded', (event) => {
   initMap();
 });
 
@@ -15,26 +15,26 @@ initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
-    } else {      
+    } else {
       self.newMap = L.map('map', {
         center: [restaurant.latlng.lat, restaurant.latlng.lng],
         zoom: 16,
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoicm1mZXJuIiwiYSI6ImNqanZxOWZ5cjl1MWUzcGxmMW9oM2V3d2QifQ.nfBNyxBOGwrtnWCzIUP1mw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'    
+        id: 'mapbox.streets'
       }).addTo(newMap);
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
   });
-}  
- 
+}
+
 /* window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
@@ -148,16 +148,25 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+
+/* Added a section to separate reviewer name and date easier */
+  const reviewHeading = document.createElement('section');
+  reviewHeading.id = 'review-heading';
+  li.appendChild(reviewHeading);
+
   const name = document.createElement('p');
+  name.id = 'name-title';
   name.innerHTML = review.name;
-  li.appendChild(name);
+  reviewHeading.appendChild(name);
 
   const date = document.createElement('p');
+  date.id = 'date-title'
   date.innerHTML = review.date;
-  li.appendChild(date);
+  reviewHeading.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.id = 'rating'
   li.appendChild(rating);
 
   const comments = document.createElement('p');
