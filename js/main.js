90de9b1b0f50bc3a6705791c334cd2e29bbd6a11
Ricardo Fernandez <v-rifern@microsoft.com>
2018-07-26 10:34:47 -0700

    HTML semantic tweaks, ARIA improvements

--- js/main.js
+++ js/main.js
@@ -160,11 +160,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = `Picture of restaurant`
+  image.alt = `Picture of ${restaurant.name} restaurant`
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
